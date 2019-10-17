f40c5c7181cddfad6566ce308d434b5b3cccb59b
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-10-17 13:16:31 +0200

    program generuje 5 losowych liczb i odnajduje największą, najmniejszą i liczy średnią

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,40 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        int i;
+        int n =5;
+        double liczba_wylosowana = 0;
+        double sum = 0;
+        double min;
+        double max;
+
+
+
+        Random random = new Random();
+        min = Math.round(100*(random.nextDouble()));
+        max = min;
+               
+
+        for (i = 0; i < n; i++) {
+
+            liczba_wylosowana = Math.round(100 * (random.nextDouble()));
+
+            System.out.print(liczba_wylosowana + ", ");
+
+            if (max < liczba_wylosowana) {max = liczba_wylosowana;}
+
+            if (liczba_wylosowana < min) {min = liczba_wylosowana;}
+
+            sum = sum + liczba_wylosowana;
+
+        }
+        System.out.println();
+        System.out.println("Max to: " + max);
+        System.out.println("Min to: " + min);
+        System.out.println("Srednia to: " + sum/n);
 
 
     }
